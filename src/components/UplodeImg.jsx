e2fd756484fe758128b987af12c3b7cbd634df74
Khakijanov <haqijonov@gmail.com>
2024-09-14 18:10:21 +0500

    update

--- src/components/UplodeImg.jsx
+++ src/components/UplodeImg.jsx
@@ -5,29 +5,32 @@ import { buttonVariants } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { PlusCircleIcon } from "lucide-react";
 import { PlusCircledIcon, PlusIcon, UpdateIcon } from "@radix-ui/react-icons";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { uplodeIMG } from "../request";
 import { toast } from "sonner";
 import { allowImgSize } from "../lib/my-utils";
+import FlowerImg from "../images/photo-1713423826277-46c52a8438d5.jpg";
 
 const UplodeImg = () => {
   const [value, setValue] = useState(null);
   const urlInput = useRef(null);
+  const [showImage, setShowImage] = useState(false);
   const handleUplodeImg = (image, type = "local") => {
     if (type === "url") {
-      setValue(image);
+      if (image !== value) {
+        toast.loading("rasim yuklanmoqda");
+        setValue(image);
+      } else {
+        toast.info("bu rasim allaqachon yuklangan");
+      }
     } else {
       if (image.size >= allowImgSize) {
         toast.error("rasim hajmi 5 mb dan katta bo'lmasligi kerek");
       } else {
-        toast.promise(uplodeIMG(image), {
-          loading: "Rasim yuklanmoqda...",
-          success: (url) => {
-            setValue(url);
-            return `rasim yuklandi`;
-          },
-          error: "Nimadir hatolik",
-        });
+        toast.loading("rasim yuklanmoqda");
+        uplodeIMG(image)
+          .then((res) => setValue(res))
+          .catch(({ message }) => toast.error(message));
       }
     }
 
@@ -35,10 +38,15 @@ const UplodeImg = () => {
       .then((res) => console.log(res))
       .catch(({ message }) => console.log(message));
   };
+
+  useEffect(() => {
+    setShowImage(true);
+    setValue(FlowerImg);
+  }, []);
   return (
     <div className="w-full">
       <Label>Rasim yuklang</Label>
-      <Tabs defaultValue="local" className="w-full">
+      <Tabs defaultValue="default" className="mb-5 w-full">
         <TabsList className="w-full">
           <TabsTrigger className="w-full" value="local">
             Local
@@ -46,9 +54,12 @@ const UplodeImg = () => {
           <TabsTrigger className="w-full" value="url">
             Url
           </TabsTrigger>
+          <TabsTrigger className="w-full" value="default">
+            dufault
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="local">
-          <Label className="mb-5 block">
+          <Label className="">
             <span
               className={`${buttonVariants({ variant: "outline" })} w-full`}
             >
@@ -62,28 +73,39 @@ const UplodeImg = () => {
               placeholder="rasim yuklash"
             />
           </Label>
-
-          {value && <img src={value} alt="img" />}
         </TabsContent>
         <TabsContent value="url">
           <Label htmlFor="url">Havola</Label>
           <div className="flex gap-5">
             <Input
               ref={urlInput}
+              e ? value : ""}
               name="file"
               type="url"
               id="url"
               placeholder="Rasim uchun havola"
             />
+
             <Button
-              onClick={() => handleUplodeImg(urlInput.current)}
+              onClick={() => handleUplodeImg(urlInput?.current.value, "url")}
               type="button"
             >
-              <PlusIcon />
+              {value ? <UpdateIcon /> : <PlusIcon />}
             </Button>
           </div>
         </TabsContent>
       </Tabs>
+      {value && showImage && (
+        <img
+          src={value}
+          alt="img"
+          onLoad={() => {
+            toast.dismiss();
+            toast.success("rasim muvofaqiyatli yuklandi");
+          }}
+          className="w-full rounded-lg"
+        />
+      )}
     </div>
   );
 };
